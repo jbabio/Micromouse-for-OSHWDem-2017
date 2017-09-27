0af190816b9e28d4e3609abfbec68cee31d62609
Juan Alberto Babío Pérez <jbabio@gmail.com>
2017-09-28 01:20:03 +0200

    Corrected initial notes.

--- Micromouse-for-OSHWDem-2017.ino
+++ Micromouse-for-OSHWDem-2017.ino
@@ -1,40 +1,36 @@
 /*
-  SharpDistSensorBasic.ino
-  Source: https://github.com/DrGFreeman/SharpDistSensor
+Micromouse for the Maze Solving contest at OSHWDem 2017(https://oshwdem.org/)
 
-  MIT License
+Note: This first version will use Left/Right algorithm to solve the maze.
 
-  Copyright (c) 2017 Julien de la Bruere-Terreault <drgfreeman@tuta.io>
+Hardware Used:
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
+    Robot base: Modified Escornabot (http://escornabot.com) (https://github.com/escornabot)
+    Main Electronics: Arduino UNO
+    Motors: 28BYJ-48
+    Motor Drivers: ULN2003 breakout board.
+    Sensors:
+        5 Sharp GP2Y0A51SK0F 2-15 cm
+        1 HMC6352 Compass (not Implemented yet)
+        1 ADXL330K Acelerometer (not Implemented yet)
 
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+Dependencies.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-*/
+    SharpDistSensor Library for Arduino, By Julien de la Bruère-Terreault. MIT License
+      (https://github.com/DrGFreeman/SharpDistSensor)
 
-/*
-  This example shows how to use the SharpDistSensor library to continuously
-  read the sensor and display the analog value and the corrseponding distance
-  in mm.
+    AccelStepper library for Arduino, Copyright (C) 2010 Mike McCauley. GPL V2
+      http://www.airspayce.com/mikem/arduino/AccelStepper/
+      Class Reference: http://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html
 
-  The library default values corresponding to the Sharp GP2Y0A60SZLF 5V sensor
-  are used.
+ Acknowledgements
 
-  See the library README for how to use pre-defined sensor models or custom
-  fit functions.  2048
+    Mike McCauley for the AccelStepper library.
+    Julien de la Bruère-Terreault for the SharpDistSensor library.
+    Andrew Kramer (https://github.com/1988kramer) for inspiration about implementing the sensor polling.
+    42Bots.com (http://42Bots.com / https://github.com/42Bots) For their example to use 28BYJ-48 motors with accelstepper library.
+    Anyone who create or share Open Source Hardware or Software.
+
 */
 
 #include <SharpDistSensor.h>
